2ec61a07b96f58ee6f11cfb5afeb484736561733
Alvin Roe <alvinroeiv@gmail.com>
2023-01-13 14:59:47 -0500

    Implemented Angular HTTP get request to User API

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 //Request Pipeline
+app.UseCors(policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 app.Run();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Project Chironn';
+export class AppComponent implements OnInit {
+  title: string = 'Project Chiron';
+  users: any; //TODO change this to a User model
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: err => console.log(err),
+      complete: () => console.log("Users loaded correctly.") //TODO update completion log or remove this
+      }
+    );
+  }
 }
